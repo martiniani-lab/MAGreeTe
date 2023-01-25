a847d9a9d95a5b715b2e447598ed442454f1c3a3
mkasiulis <m.kasiulis@gmail.com>
2023-01-25 18:46:12 -0500

    Added plotting-only options to main script and a few utils

--- magreete.py
+++ magreete.py
@@ -7,9 +7,8 @@ import colorsys
 import hickle as hkl
 import sys
 import os
-from utils import alpha_cold_atoms_2d, alpha_small_dielectric_object, plot_transmission_angularbeam, generate_square_lattice_chunk
+from utils import alpha_cold_atoms_2d, alpha_small_dielectric_object, plot_transmission_angularbeam, generate_square_lattice_chunk, generate_triangular_lattice_chunk
 from Transmission2D import Transmission2D
-from lattices import square
 
 
 import argparse
@@ -17,7 +16,7 @@ import argparse
 w = 2.1e-5 #beam waist m
 L = 100e-6 #box side length m
 
-def main(head_directory, n_cpus=1, lattice=None):
+def main(head_directory, n_cpus=1, lattice=None, just_plot = False):
     phi = 0.1
     N = 4096
     size_ratio = 1.0
@@ -62,29 +61,67 @@ def main(head_directory, n_cpus=1, lattice=None):
         N = points.shape[0]
         points *= L
 
+    elif lattice == 'triangular':
+        file_name = 'triangular'
+
+        i = 0
+
+        Nx = 71
+        Ny = 41
+        points = generate_triangular_lattice_chunk(Nx, Ny)
+        N = points.shape[0]
+        points *= L
+
+
     Ntheta = 360
     thetas = onp.arange(Ntheta)/Ntheta*2*np.pi
     meas_points = 2*L*onp.vstack([onp.cos(thetas),onp.sin(thetas)]).T
 
-    
-    solver = Transmission2D(points)
-    ETEall = []
-    ETMall = []
-    for k0, alpha in zip(k0range,alpharange):
-        EjTE, EjTM = solver.run_EM(k0, alpha, thetas, radius, n_cpus=n_cpus, self_interaction=not cold_atoms)
-        k0_ = onp.round(onp.real(k0*L/(2*onp.pi)),1)
-        params = [alpha, k0]
-        hkl.dump([onp.array(EjTE), onp.array(EjTM), onp.array(params),onp.array(points), onp.array(thetas)],file_name+'_Ek_k0_'+str(k0_)+'_'+str(i)+'.hkl')
+    if just_plot:
 
-        EkTE, EkTM = solver.calc_EM(meas_points, EjTE, EjTM, k0, alpha, thetas, n_cpus=n_cpus)
-        EkTE = np.linalg.norm(EkTE,axis=1)
-        ETEall.append(EkTE.numpy())
-        ETMall.append(EkTM.numpy())
-    ETEall = onp.array(ETEall)
-    ETMall = onp.array(ETMall)
+        ETEall = []
+        ETMall = []
+        for k0, alpha in zip(k0range,alpharange):
+            
+            k0_ = onp.round(onp.real(k0*L/(2*onp.pi)),1)
+            EjTE, EjTM, params, points, thetas = hkl.load(file_name+'_Ek_k0_'+str(k0_)+'_'+str(i)+'.hkl')
+            EjTE = np.tensor(EjTE, dtype=np.complex128)
+            EjTM = np.tensor(EjTM, dtype=np.complex128)
+            points = np.tensor(points, dtype=np.complex128)
+            thetas = onp.float64(thetas)
+            alpha, k0 = params
+            k0 = onp.float64(k0)
+            alpha = onp.complex128(alpha)
+            solver = Transmission2D(points)
 
-    TEtotal = onp.absolute(ETEall)**2
-    TMtotal = onp.absolute(ETMall)**2
+            EkTE, EkTM = solver.calc_EM(meas_points, EjTE, EjTM, k0, alpha, thetas, n_cpus=n_cpus)
+            EkTE = np.linalg.norm(EkTE,axis=1)
+            ETEall.append(EkTE.numpy())
+            ETMall.append(EkTM.numpy())
+        ETEall = onp.array(ETEall)
+        ETMall = onp.array(ETMall)
+        TEtotal = onp.absolute(ETEall)**2
+        TMtotal = onp.absolute(ETMall)**2
+
+    else:
+        solver = Transmission2D(points)
+        ETEall = []
+        ETMall = []
+        for k0, alpha in zip(k0range,alpharange):
+            EjTE, EjTM = solver.run_EM(k0, alpha, thetas, radius, n_cpus=n_cpus, self_interaction=not cold_atoms)
+            k0_ = onp.round(onp.real(k0*L/(2*onp.pi)),1)
+            params = [alpha, k0]
+            hkl.dump([onp.array(EjTE), onp.array(EjTM), onp.array(params),onp.array(points), onp.array(thetas)],file_name+'_Ek_k0_'+str(k0_)+'_'+str(i)+'.hkl')
+
+            EkTE, EkTM = solver.calc_EM(meas_points, EjTE, EjTM, k0, alpha, thetas, n_cpus=n_cpus)
+            EkTE = np.linalg.norm(EkTE,axis=1)
+            ETEall.append(EkTE.numpy())
+            ETMall.append(EkTM.numpy())
+        ETEall = onp.array(ETEall)
+        ETMall = onp.array(ETMall)
+
+        TEtotal = onp.absolute(ETEall)**2
+        TMtotal = onp.absolute(ETMall)**2
 
     plot_transmission_angularbeam(k0range, L, thetas, TMtotal, file_name, appended_string='TM')
     plot_transmission_angularbeam(k0range, L, thetas, TEtotal, file_name, appended_string='TE') 
@@ -101,7 +138,7 @@ if __name__ == '__main__':
     n_cpus = 32
     np.set_num_threads(n_cpus)
     np.device("cpu")
-    main(head_directory, n_cpus, lattice='square')
+    main(head_directory, n_cpus, lattice='triangular', just_plot=False)
     sys.exit()
 
 
